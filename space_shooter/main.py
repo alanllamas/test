859abcac38b161d13e7b8d71eceaa289cb705249
alan llamas <alanllamasg@gmail.com>
2025-04-25 20:01:41 -0600

    feat: added score and life

--- space_shooter/main.py
+++ space_shooter/main.py
@@ -11,11 +11,12 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-PLAYER_SPEED = 400
-LASER_SPEED = 500
+DATA_LAYER = 2
 PLAYER_LAYER = 1
 BG_LAYER = 0
-LASER_POWER = 20
+PLAYER_SPEED = 400
+LASER_SPEED = 500
+LASER_POWER = 30
 METEOR_LIFE = 100
 PLAYER_LIFE = 300
 METEOR_POWER = 100
@@ -34,12 +35,17 @@ class Player(pygame.sprite.Sprite):
         self.can_shoot = True
         self.shoot_time = 0
         self.life = PLAYER_LIFE
+        self.lives = []
+        self.score = 0
+        self.set__initial_lives()
+
 
     def update(self):
         self.move()
         self.collide_boundries()
         self.shoot()
         self.shoot_timer()
+        self.update_lives()
 
     def move(self):
         keys = pygame.key.get_pressed()
@@ -64,6 +70,7 @@ class Player(pygame.sprite.Sprite):
             if current_time - self.shoot_time >= self.shoot_cooldown:
                 self.can_shoot = True
                 self.shoot_time = 0
+    
     def shoot(self):
         # Create a new laser
         keys = pygame.key.get_pressed()
@@ -72,15 +79,34 @@ class Player(pygame.sprite.Sprite):
             self.shoot_time = pygame.time.get_ticks()
             laser_a = Laser(all_sprites)
             laser_a.rect.center = self.rect.center
-            laser_a.rect.x += 10
+            # laser_a.rect.x += 10
             laser_a.rect.y -= self.rect.height
-            laser_b = Laser(all_sprites)
-            laser_b.rect.center = self.rect.center
-            laser_b.rect.y -= self.rect.height
-            laser_b.rect.x -= 10
+            # laser_b = Laser(all_sprites)
+            # laser_b.rect.center = self.rect.center
+            # laser_b.rect.y -= self.rect.height
+            # laser_b.rect.x -= 10
 
-            # return laser
+    def set__initial_lives(self):
+        for i in range(0, int(self.life / 100)):
+            self.lives.append(Life((5 * i) + 5 , 10, i , all_sprites))
 
+    def update_lives(self):
+        for life in self.lives:
+            life.kill()
+        for i in range(0, int(self.life / 100)):
+            self.lives.append(Life((5 * i) + 5 , 10, i , all_sprites))
+
+class Life(pygame.sprite.Sprite):
+    def __init__(self, x, y, index, groups):
+        self._layer = PLAYER_LAYER
+        super().__init__(groups)
+        self.pre_image = pygame.image.load("assets/player.png").convert_alpha()
+        self.size = self.pre_image.get_size()
+        self.image = pygame.transform.scale(self.pre_image, (int(self.size[0]/4), int(self.size[1]/4)) )
+        self.rect = self.image.get_frect()
+        self.rect.x = x + (index * self.rect.width)
+        self.rect.y = y
+        
 class Star(pygame.sprite.Sprite):
     def __init__(self, groups, y=None):
         self._layer = BG_LAYER
@@ -137,7 +163,28 @@ class Laser(pygame.sprite.Sprite):
         if self.rect.y < 0:
             self.kill()
     
+def display_score():
+    score_surf = font.render(f"Score: {str(player.score)}" , True, (240,240,240))
+    # box_surf = pygame.Surface(score_surf.get_rect().inflate(10, 10).size, pygame.SRCALPHA)
+    # box_surf.fill(WHITE)
+    # box_surf.fill((0,0,0,100))
+    # box_surf.blit(score_surf, score_surf.get_rect(center = box_surf.get_rect().center))
+    time = int(pygame.time.get_ticks() / 1000)
+    time_surf = font.render(f"Time: {str(time)}", True, (240,240,240))
+    time_rect = time_surf.get_frect(topright = (int(SCREEN_WIDTH - 40), 50))
     
+    score_rect = score_surf.get_frect(topright = (int(SCREEN_WIDTH - 40), 20))
+    border_rect = score_rect.inflate(40, score_rect.y * 2)
+    bg_surf = pygame.Surface(border_rect.size, pygame.SRCALPHA)
+    bg_surf.fill((0,0,0,100))
+    border_rect.y = score_rect.y - 7
+    # pygame.draw.rect(screen, (0,0,0,0), bg_surf.get_frect(center = border_rect.center), 0, 5)
+    # screen.blit(bg_surf, border_rect)
+    pygame.draw.rect(screen, (240,240,240), border_rect, 2, 5)
+    screen.blit(score_surf, score_rect)
+    screen.blit(time_surf,time_rect)
+    
+
 # game setup
 pygame.init()
 running = True
@@ -145,9 +192,10 @@ running = True
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Shooter")
 clock = pygame.time.Clock()
-font = pygame.font.Font("assets/Oxanium-Bold.ttf", 36)
+font = pygame.font.Font("assets/Oxanium-Bold.ttf", 20)
+
 screen_background = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-screen_background.fill("darkgray")
+screen_background.fill("#3a2e3f")
 
 all_sprites = pygame.sprite.Group()
 player_sprites = pygame.sprite.Group()
@@ -177,8 +225,8 @@ def collitions():
                 # meteor.take_damage(laser.damage)
                 meteor.life -= laser.damage
                 if meteor.life <= 0:
+                    player.score += 1
                     meteor.kill()
-                # meteor.take_damage(10)
                 # Add explosion effect here if needed
 
     # Check for collisions between player and meteors
@@ -194,6 +242,8 @@ def collitions():
 # Game loop
 while running:
     dt = clock.tick(FPS) / 1000
+    time_surf = font.render(str(int(dt)), True, 'black')
+
     screen.fill(BLACK)
     screen.blit(screen_background, (0, 0))
     for event in pygame.event.get():
@@ -213,7 +263,9 @@ while running:
 
     #draw game state
     all_sprites.draw(screen)
-
+    display_score()
+    # screen.blit(score_surf, (int(SCREEN_WIDTH / 2), SCREEN_HEIGHT - 40))
+    # screen.blit(time_surf, (int(SCREEN_WIDTH - 40), 40))
 
     pygame.display.update()
 
