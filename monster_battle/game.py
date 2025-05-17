bed51fddb659eb062c149a4fcc9fe1aaa4aad8f5
alan llamas <alanllamasg@gmail.com>
2025-05-17 11:46:45 -0600

    added inteRactions, ui and health for both

--- monster_battle/game.py
+++ monster_battle/game.py
@@ -22,21 +22,72 @@ class Game:
     player_monster_list = ['Ivieron', 'Gulfin', 'Atrox', 'Friolera', 'Gulfin', 'Ivieron', 'Pluma']
     self.player_monsters =[ Monster(name, self.back_sprites[name], self.simple_sprites[name]) for name in player_monster_list]
     self.monster = self.player_monsters[1]
-    self.ui = UI(self.monster, self.player_monsters)
-
     monster_choice = choice(list(MONSTER_DATA.keys()))
     self.opponent_monster = Opponent(monster_choice, self.front_sprites[monster_choice], self.opponent_sprites)
+    self.ui = UI(self.monster, self.player_monsters, self.get_input)
+    self.OpponentUi = OpponentUi(self.opponent_monster)
+    self.player_active = True
+
+
+    self.timers = {
+       'player_end': Timer(1000, func= self.opponent_turn),
+       'opponent_end': Timer(1000, func= self.player_turn)
+    }
+
     self.all_sprites.add(self.monster, self.opponent_monster)
+
   def update(self, dt):
     self.all_sprites.update(dt)
-    self.ui.update(dt)
+    self.update_timers()
+    if self.player_active:
+      self.ui.update(dt)
+    self.ui.update_stats()
     pygame.display.update()
     self.screen.blit(self.bg_surfs['bg'],(0,0))
+
+  def update_timers(self):
+    for timer in self.timers.values():
+       timer.update()
+
+  def opponent_turn(self):
+    print('opponent turn')
+    attack = choice(self.opponent_monster.abilities)
+    self.apply_attack(self.monster, attack)
+    self.timers['opponent_end'].activate()
+
+  def player_turn(self):
+    self.player_active = True
 
   def draw(self):
      self.draw_monster_floor()
      self.all_sprites.draw(self.screen)
      self.ui.draw()
+     self.OpponentUi.draw()
+
+  def apply_attack(self, target, attack):
+    print(attack)
+    attack_data = ABILITIES_DATA[attack]
+    attack_type = attack_data['element']
+    element_data = ELEMENT_DATA[attack_type]
+    # print(attack_data)
+    damage = attack_data['damage']
+    element_multiplier = element_data[target.element]
+    target.health -= damage *  element_multiplier 
+    print(target.name)
+    print(f'{attack},{target.health}/{target.max_health}')
+     
+  
+  def get_input(self, state, data = None):
+    if state == 'escape':
+        self.running = False
+
+    elif state == 'attack':
+       self.apply_attack(self.opponent_monster, data)
+
+    self.player_active = False
+    self.timers['player_end'].activate()
+    print(state)
+    print(data)
 
   def draw_monster_floor(self):
     for sprite in self.all_sprites:
--- monster_battle/ui.py
+++ monster_battle/ui.py
@@ -1,16 +1,16 @@
 import pygame
 from config import *
 class UI:
-  def __init__(self, monster, player_monsters):
+  def __init__(self, monster, player_monsters, get_input):
     self.screen = pygame.display.get_surface()
     self.font = pygame.font.Font(None, 32)
     self.left = SCREEN_WIDTH / 2 - 100
     self.top = SCREEN_HEIGHT / 2 + 50
     self.monster = monster
     self.player_monsters = player_monsters
-    self.available_monsters = [monster for monster in self.player_monsters if monster != self.monster and monster.health > 0]
+    self.available_monsters = [ monster for monster in self.player_monsters if monster != self.monster and monster.health > 0 ]
     self.visible_monsters = 4
-
+    self.get_input = get_input
     self.general_options = ['attack', 'heal', 'switch', 'escape']
     self.general_index = { 'col': 0, 'row': 0 }
     
@@ -36,13 +36,21 @@ class UI:
       self.attack_index['row'] = (self.attack_index['row'] + int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])) % self.rows
       self.attack_index['col'] = (self.attack_index['col'] + int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])) % self.cols
       if keys[pygame.K_SPACE]:
-        print(self.monster.abilities[self.attack_index['col'] + self.attack_index['row'] * 2])
+        self.get_input(self.state, self.monster.abilities[self.attack_index['col'] + self.attack_index['row'] * 2])
+        self.state = 'general'
 
     elif self.state == 'switch': 
       self.switch_index = (self.switch_index + int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])) % len(self.available_monsters)
       if keys[pygame.K_SPACE]:
-        print(self.available_monsters[self.switch_index])
+        self.get_input(self.state, self.available_monsters[self.switch_index])
+        self.state = 'general'
         # self.state = self.general_options[self.general_index['col'] + self.general_index['row'] * self.player_monsters]
+    elif self.state == 'heal':
+        self.get_input('heal', self.monster)
+        self.state = 'general'
+
+    elif self.state == 'escape':
+      self.get_input('escape')
 
     if keys[pygame.K_ESCAPE]:
       self.state = 'general'
@@ -89,15 +97,35 @@ class UI:
         self.screen.blit(text_surf, text_rect)
         self.screen.blit(simple_surf, simple_rect)
 
+  def stats(self):
+    rect = pygame.FRect(self.left, self.top + -60, 250, 80)
+    pygame.draw.rect(self.screen, COLORS['white'], rect, 0, 4)
+    pygame.draw.rect(self.screen, COLORS['gray'], rect, 4, 4)
+
+    name_surf = self.font.render(self.monster.name, True, COLORS['black'])
+    name_rect = name_surf.get_frect(topleft = rect.topleft + pygame.Vector2(rect.width * 0.05, 12))
+    self.screen.blit(name_surf, name_rect)
+
+    health_rect = pygame.FRect(name_rect.left, name_rect.bottom + 10, rect.width * 0.9, 20)
+    pygame.draw.rect(self.screen, COLORS['gray'], health_rect)
+    self.draw_bar(health_rect, self.monster.health, self.monster.max_health)
+
   def general(self):
     self.generate_menu(self.cols ,self.rows , self.general_index, self.general_options)
           
   def attack(self):
     self.generate_menu(self.cols ,self.rows , self.attack_index, self.monster.abilities)
           
+  def draw_bar(self, rect, value, max_value):
+    ratio = rect.width / max_value
+    progress_rect = pygame.FRect(rect.topleft, (value * ratio, rect.height))
+    pygame.draw.rect(self.screen, COLORS['red'], progress_rect)
 
   def update(self, dt):
     self.input()
+  
+  def update_stats(self):
+    self.stats()
   
   def draw(self):
     # print(self.state)
@@ -106,3 +134,27 @@ class UI:
     if self.state == 'switch': self.switch()
     # if self.state == 'heal': self.general()
     # if self.state == 'escape': self.general()
+
+class OpponentUi:
+  def __init__(self, monster):
+    self.screen = pygame.display.get_surface()
+    self.font = pygame.font.Font(None, 32)
+    self.monster = monster
+  
+  def draw(self):
+    rect = pygame.FRect((0,0), (250, 80)).move_to(midleft = (500, self.monster.rect.centery))
+    pygame.draw.rect(self.screen, COLORS['white'], rect, 0, 4)
+    pygame.draw.rect(self.screen, COLORS['gray'], rect, 4, 4)
+
+    name_surf = self.font.render(self.monster.name, True, COLORS['black'])
+    name_rect = name_surf.get_frect(topleft = rect.topleft + pygame.Vector2(rect.width * 0.05, 12))
+    self.screen.blit(name_surf, name_rect)
+
+    health_rect = pygame.FRect(name_rect.left, name_rect.bottom + 10, rect.width * 0.9, 20)
+    pygame.draw.rect(self.screen, COLORS['gray'], health_rect)
+    self.draw_bar(health_rect, self.monster.health, self.monster.max_health)
+
+  def draw_bar(self, rect, value, max_value):
+    ratio = rect.width / max_value
+    progress_rect = pygame.FRect(rect.topleft, (value * ratio, rect.height))
+    pygame.draw.rect(self.screen, COLORS['red'], progress_rect)
